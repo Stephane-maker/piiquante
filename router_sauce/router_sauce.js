06ba93c11ade839d8c6af7952bb18dc70cfb1a33
stephane-maker <contact@mydevproject.fr>
2022-05-16 23:21:44 +0200

    ajout du middleware pour les token modifie supprime selon le token de celui qui as crée la sauce ajout de multer pour ajouter des images sur l'application et affichage des images enregistre sur le server

--- router_sauce/router_sauce.js
+++ router_sauce/router_sauce.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const controllerSauce = require("../controllers/controller_sauce");
-
+const authenticateToken = require("../middleware/auth_token");
+const multer = require("../middleware/multer_config")
 
 const router = express.Router();
 
-router.post('/', controllerSauce.createSauce);
-router.get("/:id", controllerSauce.selectOneSauce);
-router.get('/', controllerSauce.selectAllSauce);
+router.post('/', authenticateToken, multer, controllerSauce.createSauce);
+router.get("/:id", authenticateToken, controllerSauce.selectOneSauce);
+router.get('/', authenticateToken, controllerSauce.selectAllSauce);
 
 module.exports = router;
