43d791e527522a5da703692297d5bd4f1631cf0d
stephane-maker <contact@mydevproject.fr>
2022-05-18 22:03:06 +0200

    ajout de la route PUT and DELETE

--- router_sauce/router_sauce.js
+++ router_sauce/router_sauce.js
@@ -8,5 +8,7 @@ const router = express.Router();
 router.post('/', authenticateToken, multer, controllerSauce.createSauce);
 router.get("/:id", authenticateToken, controllerSauce.selectOneSauce);
 router.get('/', authenticateToken, controllerSauce.selectAllSauce);
+router.put('/:id', authenticateToken, controllerSauce.modifiySauce);
+router.delete("/:id", authenticateToken, controllerSauce.deleteSauce);
 
 module.exports = router;
