3389ce0f5533025f69527ede2b7f6c3fb5427d98
stephane-maker <contact@mydevproject.fr>
2022-06-06 02:11:13 +0200

    mise a jour de la fonction delete , et progression sur le systeme de like et dislike

--- controllers/controller_sauce.js
+++ controllers/controller_sauce.js
@@ -2,12 +2,11 @@ const Sauce = require("../models/sauce");
 const fs = require("fs");
 const { cp } = require("fs/promises");
 
-require("dotenv").config()
+require("dotenv").config();
 
 exports.createSauce = (req, res, next) => {
     const SauceObject = JSON.parse(req.body.sauce)
     delete SauceObject._id
-
     SauceObject.idCompare = req.auth.userId;
     const sauce = new Sauce({
         ...SauceObject,
@@ -33,7 +32,6 @@ exports.selectAllSauce = (req, res, next) => {
     Sauce.find()
         .then(sauce => res.status(200).json(sauce))
         .catch(error => res.status(400).json({ error }));
-
 };
 exports.modifySauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
@@ -48,72 +46,71 @@ exports.modifySauce = (req, res, next) => {
 };
 
 exports.deleteSauce = (req, res, next) => {
-
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => {
-            const filename = sauce.imageUrl.split("/image/")[1];
-            fs.unlink(`image/${filename}`, () => {
-                Sauce.deleteOne({ _id: req.params.id })
-                    .then(() => res.status(200).json({ message: "Object deleted" }))
-                    .catch((error) => res.status(400).json({ error }))
-            })
+            if (sauce.idCompare === req.auth.userId) {
+                const filename = sauce.imageUrl.split("/image/")[1];
+                fs.unlink(`image/${filename}`, () => {
+                    Sauce.deleteOne({ _id: req.params.id })
+                        .then(() => res.status(200).json({ message: "Object deleted" }))
+                        .catch((error) => res.status(400).json({ error }))
+                })
+            } else {
+                return res.status(400).json({ message: "unauthorized request" })
+            }
         })
-        .catch(res.status(400).json({ message: "unauthorized request" }));
-
 };
 
 exports.likedSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then((sauce) => {
-            console.log(req.body)
-            if (req.body.like === 1 && !sauce.usersLiked.includes(req.auth.userId) && !sauce.usersDisliked.includes(req.auth.userId)) {
-                Sauce.updateMany({ "_id": req.params.id }, { '$push': { 'usersLiked': req.auth.userId } })
+            function DeleteUserLiked() {
+                Sauce.updateMany({ _id: req.params.id }, { "$pull": { "usersLiked": req.auth.userId } })
                     .then(() => res.status(200).json({ message: "Object liked" }))
                     .catch(() => res.status(400).json({ message: "An error has occurred" }))
-
             }
-            if (req.body.like === 1 && sauce.usersLiked.includes(req.auth.userId)) {
-                Sauce.updateMany({ "_id": req.params.id }, { '$pull': { "usersLiked": req.auth.userId } })
+
+            function deleteUserDisLiked() {
+                Sauce.updateMany({ _id: req.params.id }, { '$pull': { 'usersDisliked': req.auth.userId } })
                     .then(() => res.status(200).json({ message: "Object liked" }))
-                    .catch(() => res.status(400).json({ message: "An error has occurred" }));
-
-
+                    .catch(() => res.status(400).json({ message: "An error has occurred" }))
             }
-            if (req.body.like === -1 && sauce.usersLiked.includes(req.auth.userId)) {
-                return false
 
+            if (req.body.like === 1) {
+                if (!sauce.usersLiked.includes(req.auth.userId)) {
+                    Sauce.updateMany({ _id: req.params.id }, { "$push": { "usersLiked": req.auth.userId } })
+                        .then(() => res.status(200).json({ message: "Object liked" }))
+                        .catch(() => res.status(400).json({ message: "An error has occurred" }))
+                }
+                if (sauce.usersLiked.includes(req.auth.userId)) {
+                    DeleteUserLiked()
+                }
+                if (sauce.usersDisliked.includes(req.auth.userId)) {
+                    deleteUserDisLiked()
+                }
+            }
 
+            if (req.body.like === -1) {
+                if (!sauce.usersDisliked.includes(req.auth.userId)) {
+                    Sauce.updateMany({ _id: req.params.id }, { '$push': { 'usersDisliked': req.auth.userId } })
+                        .then(() => res.status(200).json({ message: "Object liked" }))
+                        .catch(() => res.status(400).json({ message: "An error has occurred" }))
+                }
+                if (sauce.usersDisliked.includes(req.auth.userId)) {
+                    deleteUserDisLiked()
+                }
+                if (sauce.usersLiked.includes(req.auth.userId)) {
+                    DeleteUserLiked()
+                }
             }
 
             if (req.body.like === 0) {
                 if (sauce.usersLiked.includes(req.auth.userId)) {
-                    Sauce.updateMany({ "_id": req.params.id }, { '$pull': { "usersLiked": req.auth.userId } })
-                        .then(() => res.status(200).json({ message: "Object disliked" }))
-                        .catch(() => res.status(400).json({ message: "An error has occurred" }))
-                }
-                if (!sauce.usersLiked.includes(req.auth.userId)) {
-                    Sauce.updateMany({ "_id": req.params.id }, { '$pull': { "usersDisliked": req.auth.userId } })
-                        .then(() => res.status(200).json({ message: "Object disliked" }))
-                        .catch(() => res.status(400).json({ message: "An error has occurred" }))
+                    DeleteUserLiked()
+                } else {
+                    deleteUserDisLiked()
                 }
             }
-
-            if (req.body.like === -1 && !sauce.usersDisliked.includes(req.auth.userId) && !sauce.usersLiked.includes(req.auth.userId)) {
-                Sauce.updateMany({ "_id": req.params.id }, { '$push': { 'usersDisliked': req.auth.userId } })
-                    .then(() => res.status(200).json({ message: "Object liked" }))
-                    .catch(() => res.status(400).json({ message: "An error has occurred" }))
-            }
-            if (req.body.like === -1 && sauce.usersDisliked.includes(req.auth.userId)) {
-                Sauce.updateMany({ "_id": req.params.id }, { '$pull': { 'usersDisliked': req.auth.userId } })
-                    .then(() => res.status(200).json({ message: "Object liked" }))
-                    .catch(() => res.status(400).json({ message: "An error has occurred" }))
-            }
-
-
         })
         .catch((error) => res.status(400).json({ error }))
-}
-
-exports.disLikeSauce = (req, res, next) => {
-    console.log("ok");
 }
