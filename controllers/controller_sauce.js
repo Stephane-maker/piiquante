4e094f3c63746e87f5980a16e9d16996123f1ae5
stephane-maker <contact@mydevproject.fr>
2022-06-08 22:34:41 +0200

    correction de l'userId sur le controller_user et dans le fichier auth_token mise a jour de la requete modifySauce mise a jour du systeme de like et dislikle

--- controllers/controller_sauce.js
+++ controllers/controller_sauce.js
@@ -5,9 +5,10 @@ const { cp } = require("fs/promises");
 require("dotenv").config();
 
 exports.createSauce = (req, res, next) => {
+
     const SauceObject = JSON.parse(req.body.sauce)
     delete SauceObject._id
-    SauceObject.idCompare = req.auth.userId;
+    SauceObject.userId = req.auth.userId;
     const sauce = new Sauce({
         ...SauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/image/${req.file.filename}`
@@ -19,36 +20,42 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.selectOneSauce = (req, res, next) => {
+
     Sauce.findOne({ _id: req.params.id })
-        .then(sauce => {
-            sauce.likes = sauce.usersLiked.length;
-            sauce.dislikes = sauce.usersDisliked.length;
-            return res.status(200).json(sauce);
-        })
-        .catch(error => res.status(400).json({ error }));
+
+    .then(sauce => {
+        sauce.likes = sauce.usersLiked.length;
+        sauce.dislikes = sauce.usersDisliked.length;
+
+        return res.status(200).json(sauce);
+    })
+
+    .catch(error => res.status(400).json({ error }));
 };
 
 exports.selectAllSauce = (req, res, next) => {
+
     Sauce.find()
         .then(sauce => res.status(200).json(sauce))
         .catch(error => res.status(400).json({ error }));
 };
 exports.modifySauce = (req, res, next) => {
+
     Sauce.findOne({ _id: req.params.id })
-        .then((sauce) => {
-            if (req.auth.userId === sauce.idCompare) {
-                Sauce.updateOne({ _id: req.params.id }, {...req.body, id: req.params.id })
-                return res.status(200).json({ message: "Object modified" })
-            } else {
-                return (res.status(400).json({ message: "unauthorized request" }));
-            }
-        })
-};
+
+    console.log(req.body)
+    Sauce.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+        .catch(error => res.status(400).json({ error }));
+
+
+
+}
 
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => {
-            if (sauce.idCompare === req.auth.userId) {
+            if (sauce.userId === req.auth.userId) {
                 const filename = sauce.imageUrl.split("/image/")[1];
                 fs.unlink(`image/${filename}`, () => {
                     Sauce.deleteOne({ _id: req.params.id })
@@ -64,29 +71,22 @@ exports.deleteSauce = (req, res, next) => {
 exports.likedSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then((sauce) => {
-            function DeleteUserLiked() {
-                Sauce.updateMany({ _id: req.params.id }, { "$pull": { "usersLiked": req.auth.userId } })
-                    .then(() => res.status(200).json({ message: "Object liked" }))
-                    .catch(() => res.status(400).json({ message: "An error has occurred" }))
-            }
-
-            function deleteUserDisLiked() {
-                Sauce.updateMany({ _id: req.params.id }, { '$pull': { 'usersDisliked': req.auth.userId } })
-                    .then(() => res.status(200).json({ message: "Object liked" }))
-                    .catch(() => res.status(400).json({ message: "An error has occurred" }))
-            }
-
             if (req.body.like === 1) {
                 if (!sauce.usersLiked.includes(req.auth.userId)) {
                     Sauce.updateMany({ _id: req.params.id }, { "$push": { "usersLiked": req.auth.userId } })
                         .then(() => res.status(200).json({ message: "Object liked" }))
                         .catch(() => res.status(400).json({ message: "An error has occurred" }))
                 }
+            }
+            if (req.body.like === 0) {
                 if (sauce.usersLiked.includes(req.auth.userId)) {
-                    DeleteUserLiked()
-                }
-                if (sauce.usersDisliked.includes(req.auth.userId)) {
-                    deleteUserDisLiked()
+                    Sauce.updateMany({ _id: req.params.id }, { "$pull": { "usersLiked": req.auth.userId } })
+                        .then(() => res.status(200).json({ message: "Object liked" }))
+                        .catch(() => res.status(400).json({ message: "An error has occurred" }))
+                } else {
+                    Sauce.updateMany({ _id: req.params.id }, { '$pull': { 'usersDisliked': req.auth.userId } })
+                        .then(() => res.status(200).json({ message: "Object liked" }))
+                        .catch(() => res.status(400).json({ message: "An error has occurred" }))
                 }
             }
 
@@ -95,20 +95,6 @@ exports.likedSauce = (req, res, next) => {
                     Sauce.updateMany({ _id: req.params.id }, { '$push': { 'usersDisliked': req.auth.userId } })
                         .then(() => res.status(200).json({ message: "Object liked" }))
                         .catch(() => res.status(400).json({ message: "An error has occurred" }))
-                }
-                if (sauce.usersDisliked.includes(req.auth.userId)) {
-                    deleteUserDisLiked()
-                }
-                if (sauce.usersLiked.includes(req.auth.userId)) {
-                    DeleteUserLiked()
-                }
-            }
-
-            if (req.body.like === 0) {
-                if (sauce.usersLiked.includes(req.auth.userId)) {
-                    DeleteUserLiked()
-                } else {
-                    deleteUserDisLiked()
                 }
             }
         })
--- middleware/auth_token.js
+++ middleware/auth_token.js
@@ -1,13 +1,15 @@
-const jwt = require('jsonwebtoken');
+const jsonWebToken = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-        const userId = decodedToken.UserId;
+        const decodedToken = jsonWebToken.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
 
-        req.auth = { userId: userId };
+        req.auth = { userId };
+
         if (req.body.userId && req.body.userId !== userId) {
+
             throw 'Invalid user ID';
         } else {
             next();
